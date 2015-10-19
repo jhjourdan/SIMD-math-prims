21c806eafec6026ba6ef76aca9770e20cfa5a3a8
Jacques-Henri Jourdan <jacques-henri.jourdan@inria.fr>
2015-10-19 12:37:03 +0200

    A bit more documentation.

--- simd_math_prims.h
+++ simd_math_prims.h
@@ -50,6 +50,12 @@ inline float expapprox(float val) {
   xu.i = val4i & 0x7F800000;
   xu2.i = (val4i & 0x7FFFFF) | 0x3F800000;
   b = xu2.f;
+
+  /* Generated in Sollya with:
+     > f=remez(1, 4, [1,2], exp(-(x-1)*log(2)), 1e-10);
+     > plot(exp((x-1)*log(2))/f-1, [1,2]);
+     > f;
+  */
   return
     xu.f * (0.509964287281036376953125f + b *
             (0.3120158612728118896484375f + b *
@@ -67,10 +73,19 @@ inline float logapprox_cont(float val) {
   float exp, addcst, x;
   valu.f = val;
   exp = valu.i >> 23;
+  /* 89.970756366f = 127 * log(2) - constant term of polynomial */
   addcst = val > 0 ? -89.970756366f : -(float)INFINITY;
   valu.i = (valu.i & 0x7FFFFF) | 0x3F800000;
   x = valu.f;
 
+
+  /* Generated in Sollya using :
+    > f = remez(log(x)-(x-1)*log(2),
+            [|1,(x-1)*(x-2), (x-1)*(x-2)*x, (x-1)*(x-2)*x*x,
+              (x-1)*(x-2)*x*x*x|], [1,2], 1, 1e-8);
+    > plot(f+(x-1)*log(2)-log(x), [1,2]);
+    > f+(x-1)*log(2)
+ */
   return
     x * (3.529304993f + x * (-2.461222105f +
       x * (1.130626167f + x * (-0.288739945f +
