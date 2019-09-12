8067be01b5ad0c9e68c850174fac010429813d37
Jacques-Henri Jourdan <jacques-henri.jourdan@normalesup.org>
2019-09-12 23:17:58 +0200

    More precise double versions of the functions.

--- test_fun.cpp
+++ test_fun.cpp
@@ -15,18 +15,21 @@ using namespace std;
 const int N_samp = 1<<10;
 
 float vals_test[N_samp];
+double vals_test_d[N_samp];
 
 float buf[N_samp];
+double buf_d[N_samp];
 
-void compare_fun_f(float (*f1)(float), float (*f2)(float), float low, float high,
+template<typename FL>
+void compare_fun_f(FL (*f1)(FL), FL (*f2)(FL), FL low, FL high,
                    bool abs, bool rel) {
   double sum_err = 0, sum_abs_err = 0, sum_sq_err = 0,
     min_err = +INFINITY, max_err = -INFINITY;
   double sum_rel_err = 0, sum_abs_rel_err = 0, sum_sq_rel_err = 0,
     min_rel_err = +INFINITY, max_rel_err = -INFINITY;
   for(int i = 0; i < N_samp; i++) {
-    float x = high*vals_test[i]+low*(1-vals_test[i]);
-    float y1 = f1(x), y2 = f2(x);
+    double x = high*vals_test[i]+low*(1-vals_test[i]);
+    FL y1 = f1(x), y2 = f2(x);
     double err = (double)y1 - y2;
     sum_err += err;
     sum_abs_err += fabs(err);
@@ -64,13 +67,13 @@ void compare_fun_f(float (*f1)(float), float (*f2)(float), float low, float high
     compare_fun_f(f1, f2, low, high, abs, rel);                         \
   }
 
-#define bench_fun(f, cycles) {                                          \
+#define bench_fun(f, cycles, buf, vals_test) {                          \
     printf("Benchmarking " #f "...");                                   \
     clock_t begin = clock();                                            \
     for(int i = 0; i < cycles; i++) {                                   \
       for(int j = 0; j < N_samp; j++)                                   \
         buf[j] = f(vals_test[j]);                                       \
-      __asm__(""); /* Make sure the above computation is not dropped */ \
+      __asm__(""); /* Make sure the computation above is not dropped */ \
     }                                                                   \
     clock_t end = clock();                                              \
     double dt = double(end - begin) / CLOCKS_PER_SEC;                   \
@@ -78,39 +81,61 @@ void compare_fun_f(float (*f1)(float), float (*f2)(float), float low, float high
     printf("    %.1fM/s\n", throughput/1e6);                            \
 }
 
+#define bench_fun_f(f, cycles)  \
+  bench_fun(f, cycles, buf, vals_test);
+
+#define bench_fun_d(f, cycles)  \
+  bench_fun(f, cycles, buf_d, vals_test_d);
+
 int main() {
   mt19937 r;
-  uniform_real_distribution<float> d;
+  uniform_real_distribution<double> d;
 
   for(int i = 0; i < N_samp; i++)
     vals_test[i] = d(r);
 
   printf("Sin functions:\n--------------\n");
-  compare_fun(sinapprox, sinf, -M_PI, M_PI, true, false);
+  compare_fun(sinapprox, sinf, -(float)M_PI, (float)M_PI, true, false);
   printf("\n");
-  bench_fun(sinf, 100000);
-  bench_fun(sinapprox, 1000000L);
+  compare_fun(sinapprox_d, sin, -(double)M_PI, (double)M_PI, true, false);
+  printf("\n");
+  bench_fun_f(sinf, 100000);
+  bench_fun_f(sinapprox, 1000000L);
+  bench_fun_d(sin, 100000);
+  bench_fun_d(sinapprox_d, 1000000L);
 
   printf("\n\nCos functions:\n--------------\n");
-  compare_fun(cosapprox, cosf, -M_PI, M_PI, true, false);
+  compare_fun(cosapprox, cosf, -(float)M_PI, (float)M_PI, true, false);
   printf("\n");
-  bench_fun(cosf, 100000);
-  bench_fun(cosapprox, 1000000L);
+  compare_fun(cosapprox_d, cos, -(double)M_PI, (double)M_PI, true, false);
+  printf("\n");
+  bench_fun_f(cosf, 100000);
+  bench_fun_f(cosapprox, 1000000L);
+  bench_fun_d(cos, 100000);
+  bench_fun_d(cosapprox_d, 1000000L);
 
   printf("\n\nLog functions:\n--------------\n");
-  compare_fun(logapprox, logf, 1e-10, 10, true, false);
+  compare_fun(logapprox, logf, 1e-10f, 10.f, true, false);
   printf("\n");
-  compare_fun(icsi_log, logf, 1e-10, 10, true, false);
+  compare_fun(icsi_log, logf, 1e-10f, 10.f, true, false);
   printf("\n");
-  bench_fun(logf, 100000);
-  bench_fun(icsi_log, 100000L);
-  bench_fun(logapprox, 1000000L);
+  compare_fun(logapprox_d, log, 1e-10, 10., true, false);
+  printf("\n");
+  bench_fun_f(logf, 100000);
+  bench_fun_f(icsi_log, 100000L);
+  bench_fun_f(logapprox, 1000000L);
+  bench_fun_f(log, 100000L);
+  bench_fun_f(logapprox_d, 1000000L);
 
   printf("\n\nExp functions:\n--------------\n");
-  compare_fun(expapprox, expf, -10, 10, false, true);
+  compare_fun(expapprox, expf, -10.f, 10.f, false, true);
   printf("\n");
-  bench_fun(expf, 100000);
-  bench_fun(expapprox, 1000000L);
+  compare_fun(expapprox_d, exp, -10., 10., false, true);
+  printf("\n");
+  bench_fun_f(expf, 100000);
+  bench_fun_f(expapprox, 1000000L);
+  bench_fun_d(exp, 100000);
+  bench_fun_d(expapprox_d, 1000000L);
 
   return 0;
 }
